28a29edfab6dd298d93ab7e7c576de37a1367cac
94toman <94toman@gmail.com>
2022-07-19 15:30:07 +0200

    testing image blur

--- components/Cards/_Porad/Porad.tsx
+++ components/Cards/_Porad/Porad.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import { truncateString } from '../../functions';
 import styles from './Porad.module.scss';
 
@@ -17,13 +17,23 @@ export interface IPorad {
 }
 
 const Porad: React.FC<IPorad> = React.forwardRef(({ porad }) => {
+	const [src, setSrc] = useState(porad.logo); //image loading error handling
+
 	return (
 		<>
 			<Link href={`porady/${porad.id}`}>
 				<div className={styles.cardWrapper}>
 					<div className={styles.card}>
 						<div className={styles.card_img}>
-							<Image src={`${porad.logo}`} layout="intrinsic" width={450} height={350} />
+							<Image
+								src={src}
+								layout="intrinsic"
+								width={450}
+								height={350}
+								onError={() => setSrc('/public/placeholder.jpg')}
+								placeholder="blur"
+								blurDataURL="/public/placeholder.jpg"
+							/>
 						</div>
 						<div className={styles.card_content}>
 							{porad.lead.length > 0 ? <p>{truncateString(porad.lead, 240)}</p> : <></>}
